8b38709961be4974bbdf93dd2a69430c4fe9b581
Oumayma azmi <oumaymaazmi@gmail.com>
2025-06-22 19:46:39 +0100

    add unit tests for ProductService

---
+++ task-service/src/test/java/com/crud/task/service/services/product/ProductServiceTest.java
@@ -0,0 +1,70 @@
+package com.crud.task.service.services.product;
+
+import com.crud.task.domain.exception.FunctionalException;
+import com.crud.task.domain.pojo.Product;
+import com.crud.task.domain.port.IProductRepository;
+import com.crud.task.service.config.MessageResolver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+    private static final String ERROR_PRICE_REQUIRED = "error.product.priceRequired";
+
+    @Mock
+    private IProductRepository productRepository;
+
+    @Mock
+    private MessageResolver messageResolver;
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Test
+    void shouldSaveProduct() {
+        Product product = buildDefaultProduct();
+        productService.create(product);
+        verify(productRepository).save(product);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPriceNull() {
+        Product product = buildDefaultProduct();
+        product.setPrice(null);
+        when(messageResolver.get(ERROR_PRICE_REQUIRED)).thenReturn("Product price is required");
+
+        FunctionalException exception = assertThrows(FunctionalException.class, () -> productService.create(product));
+        assertEquals("Product price is required", exception.getMessage());
+    }
+
+    @Test
+    void shouldReturnProduct() {
+        Long id = 1L;
+        Product expected = buildDefaultProduct();
+        expected.setId(id);
+        when(productRepository.findById(id)).thenReturn(Optional.of(expected));
+
+        Product result = productService.findById(id);
+
+        assertNotNull(result);
+        assertEquals(result, expected);
+    }
+
+    Product buildDefaultProduct() {
+        return Product.builder()
+                .name("Laptop")
+                .price(BigDecimal.valueOf(1200))
+                .build();
+    }
+}
